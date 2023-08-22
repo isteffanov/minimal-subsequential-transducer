d56791886c18e738038d232fa97985f475b0ea09
Ivan Stefanov <isteffanov@gmail.com>
2023-08-22 20:16:50 +0300

    Graph now shows final states, state output and initial output

--- graphs.py
+++ graphs.py
@@ -1,11 +1,11 @@
 import os
 import pygraphviz as pgv
-# from transducer import Transducer
 
 class Transducer:
     pass
 
 def draw_transducer(T: Transducer, filename: str) -> None:
+    
     edge_labels = {}
     for f, l in T.deltaT:
         t = T.deltaT[(f, l)]
@@ -17,11 +17,20 @@ def draw_transducer(T: Transducer, filename: str) -> None:
     
     edges = edge_labels.keys()
     
-    G = pgv.AGraph(strict=False, directed=True)
+    G = pgv.AGraph(strict=False, directed=True, comment=T.iota)
     
     for (f, t) in edges:
         G.add_edge(f, t, label=edge_labels[(f, t)])
-        
+   
+    G.add_node(1, label=T.iota if len(T.iota) > 0 else "''")
+    
+    
+    
+    for state in T.F:
+        n = G.get_node(state)
+        n.attr["peripheries"] = "2"    
+        n.attr["label"] += f'\n{T.psi[state]}'
+    
     G.layout(prog="dot")
     
     os.makedirs("plots", exist_ok=True)
